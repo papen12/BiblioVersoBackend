453048016aa9864033a6f978d3ca6f8cc8f6857a
papen12 <papenfabri2.0@gmail.com>
2025-11-25 17:43:40 -0400

    change

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
-
 import cors from 'cors';
+
 import multaRouter from '../Routes/MultaRouter';
 import reservaRouter from '../Routes/ReservaRouter';
 import autorRouter from '../Routes/AutorRouter';
@@ -14,46 +14,31 @@ import stockRouter from '../Routes/StockRouter';
 import signUpRouter from '../Routes/signUpRouter';
 import libroAutorRouter from '../Routes/LibroAutorRouter';
 
-
-
-
 dotenv.config();
 
-
-
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 app.use(cors({
   origin: process.env.FRONTEND_URL || '*'
 }));
 app.use(express.json());
 
-
-
 app.use('/api/persona', personaRouter);
 app.use('/api/multa', multaRouter);
 app.use('/api/reserva', reservaRouter);
 app.use('/api/administrador', administradorRouter);
-app.use('/api/cliente',clienteRouter)
-app.use('/api/autor', autorRouter)
-app.use('/api/auth',authRouter)
-app.use('/api/libro',libroRouter)
+app.use('/api/cliente', clienteRouter);
+app.use('/api/autor', autorRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/libro', libroRouter);
 app.use('/api/signup', signUpRouter);
 app.use('/api/stock', stockRouter);
-app.use('/api/LA',libroAutorRouter)
-console.log("Ruta /api/stock cargada correctamente");
+app.use('/api/LA', libroAutorRouter);
+
 app.get('/api', (_req, res) => {
   res.json({ message: "Hola desde Express!" });
 });
 
-
-
-//parte cambiada para configurar vagrant
-// app.listen(PORT, () => {
-//   console.log(`Servidor corriendo en http://localhost:${PORT}`);
-// });
-
-app.listen(5000, '0.0.0.0', () => {
-  console.log("Servidor corriendo en http://localhost:5000");
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
